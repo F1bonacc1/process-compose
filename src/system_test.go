4c9d6eacf924e437f0ba7c2a2cf4d8e5b19cb796
Berger Eugene <bergere@gmail.com>
2022-04-07 00:32:28 +0300

    auto discover tests

--- src/system_test.go
+++ src/system_test.go
@@ -23,3 +23,44 @@ func TestSystem_TestFixtures(t *testing.T) {
 		})
 	}
 }
+
+func Test_autoDiscoverComposeFile(t *testing.T) {
+	type args struct {
+		pwd string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "Should not find",
+			args: args{
+				pwd: "../fixtures",
+			},
+			want:    "",
+			wantErr: true,
+		},
+		{
+			name: "Should find process-compose.yaml",
+			args: args{
+				pwd: "../",
+			},
+			want:    "../process-compose.yaml",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := autoDiscoverComposeFile(tt.args.pwd)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("autoDiscoverComposeFile() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("autoDiscoverComposeFile() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
