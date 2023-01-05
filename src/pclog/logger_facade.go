eb277eebd001ca91975db80b780f5f1379b113ca
Berger Eugene <bergere@gmail.com>
2023-01-06 01:09:30 +0200

    Fix unified log race condition

--- src/pclog/logger_facade.go
+++ src/pclog/logger_facade.go
@@ -3,14 +3,24 @@ package pclog
 import (
 	"bufio"
 	"os"
+	"sync"
 
 	"github.com/rs/zerolog"
 )
 
 type PCLog struct {
-	logger zerolog.Logger
-	writer *bufio.Writer
-	file   *os.File
+	logger       zerolog.Logger
+	writer       *bufio.Writer
+	file         *os.File
+	logEventChan chan logEvent
+	wg           sync.WaitGroup
+}
+
+type logEvent struct {
+	message string
+	process string
+	replica int
+	isErr   bool
 }
 
 func NewLogger(outputPath string) *PCLog {
@@ -20,29 +30,66 @@ func NewLogger(outputPath string) *PCLog {
 	}
 	writer := bufio.NewWriter(f)
 
-	return &PCLog{
-		writer: writer,
-		file:   f,
-		logger: zerolog.New(writer),
+	log := &PCLog{
+		writer:       writer,
+		file:         f,
+		logger:       zerolog.New(writer),
+		logEventChan: make(chan logEvent, 100),
 	}
+	log.wg.Add(1)
+	go log.runCollector()
+	return log
 }
 
-func (l PCLog) Info(message string, process string, replica int) {
+func (l *PCLog) Info(message string, process string, replica int) {
+	l.logEventChan <- logEvent{
+		message: message,
+		process: process,
+		replica: replica,
+		isErr:   false,
+	}
+}
+func (l *PCLog) info(message string, process string, replica int) {
 	l.logger.Info().
 		Str("process", process).
 		Int("replica", replica).
 		Msg(message)
-
 }
 
-func (l PCLog) Error(message string, process string, replica int) {
+func (l *PCLog) Error(message string, process string, replica int) {
+	l.logEventChan <- logEvent{
+		message: message,
+		process: process,
+		replica: replica,
+		isErr:   true,
+	}
+}
+
+func (l *PCLog) error(message string, process string, replica int) {
 	l.logger.Error().
 		Str("process", process).
 		Int("replica", replica).
 		Msg(message)
 }
 
-func (l PCLog) Close() {
+func (l *PCLog) Close() {
+	close(l.logEventChan)
+	l.wg.Wait()
 	l.writer.Flush()
 	l.file.Close()
+}
+
+func (l *PCLog) runCollector() {
+	for {
+		event, open := <-l.logEventChan
+		if !open {
+			break
+		}
+		if event.isErr {
+			l.error(event.message, event.process, event.replica)
+		} else {
+			l.info(event.message, event.process, event.replica)
+		}
+	}
+	l.wg.Done()
 }
