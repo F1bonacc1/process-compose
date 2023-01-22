db93aa457ffa6a778ab31cc4337db2acfb671216
Berger Eugene <bergere@gmail.com>
2023-01-22 22:17:55 +0200

    update default.nix vendorSha

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@ let pkg = "github.com/f1bonacc1/process-compose/src/config";
 in
 buildGoModule rec {
   pname = "process-compose";
-  version = "0.29.7";
+  version = "0.40.0";
 
 
   src = lib.cleanSource ./.;
