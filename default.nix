10b7e80c89f1cd8edeb915229438cf927beab985
Berger Eugene <bergere@gmail.com>
2023-01-31 23:35:48 +0200

    Bump default.nix to v0.40.1

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@ let pkg = "github.com/f1bonacc1/process-compose/src/config";
 in
 buildGoModule rec {
   pname = "process-compose";
-  version = "0.40.0";
+  version = "0.40.1";
 
 
   src = lib.cleanSource ./.;
