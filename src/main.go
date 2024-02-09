4268138b87e894464d0fde7bc9a08b542470b8d3
Berger Eugene <bergere@gmail.com>
2024-02-09 19:13:12 +0200

    Remove Profiler

--- src/main.go
+++ src/main.go
@@ -2,14 +2,8 @@ package main
 
 import (
 	"github.com/f1bonacc1/process-compose/src/cmd"
-	"log"
-	"net/http"
-	_ "net/http/pprof"
 )
 
 func main() {
-	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
 	cmd.Execute()
 }
