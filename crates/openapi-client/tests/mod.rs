ba864820731347936a0b4eabf9975b60df789549
dzmitry-lahoda <dzmitry@lahoda.pro>
2024-09-28 23:23:39 +0100

    workz

--- crates/openapi-client/tests/mod.rs
+++ crates/openapi-client/tests/mod.rs
@@ -3,5 +3,5 @@ use process_compose_openapi_client;
 #[tokio::test]
 async fn compiles_but_errors_with_bad_url() {
     let client = process_compose_openapi_client::Client::new("locahost:8080");
-    client.get_hostname().await.expect_err("errors on bad url");
+    client.get_host_name().await.expect_err("errors on bad url");
 }
