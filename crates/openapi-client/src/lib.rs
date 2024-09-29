0860d08c853c0a1532ed874e4e9932bd6553f587
dzmitry-lahoda <dzmitry@lahoda.pro>
2024-09-29 17:54:03 +0100

    works

--- crates/openapi-client/src/lib.rs
+++ crates/openapi-client/src/lib.rs
@@ -1,17 +1,2 @@
 use progenitor::generate_api;
-generate_api!(
-  spec = "../../src/docs/swagger.3.json",
-  derives = [  ],
-);
-
-// https://github.com/oxidecomputer/progenitor/issues/694#issuecomment-1901006117
-
-// Compiling process-compose-openapi-client v0.1.0 (/home/dz/github.com/F1bonacc1/process-compose/crates/openapi-client)
-// error: generation error for ../../src/docs/swagger.3.json: unexpected or unhandled format in the OpenAPI document path /hostname is missing operation ID
-//  --> src/lib.rs:2:22
-//   |
-// 2 | generate_api!(spec = "../../src/docs/swagger.3.json");
-//   |                      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-// error: could not compile `process-compose-openapi-client` (lib) due to 1 previous error
-// warning: build failed, waiting for other jobs to finish...
+generate_api!(spec = "../../src/docs/swagger.3.json", derives = [],);
--- crates/openapi-client/tests/mod.rs
+++ crates/openapi-client/tests/mod.rs
@@ -3,5 +3,11 @@ use process_compose_openapi_client;
 #[tokio::test]
 async fn compiles_but_errors_with_bad_url() {
     let client = process_compose_openapi_client::Client::new("locahost:8080");
-    client.get_host_name().await.expect_err("errors on bad url");
+    match client.get_process_info("process-compose").await {
+        Ok(response) => {
+            let _name = &response.name;
+            unreachable!("errors on bad url");
+        }
+        Err(_) => {}
+    }
 }
