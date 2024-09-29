835d19432b1262aa7c24b1412c12eb33c5015545
dzmitry-lahoda <dzmitry@lahoda.pro>
2024-09-29 18:45:30 +0100

    clean up

--- crates/openapi-client/build.rs
+++ crates/openapi-client/build.rs
@@ -1,16 +1,13 @@
 fn main() {
-    let src = "../../src/docs/swagger.3.json";
+    let src = "swagger.3.json";
     println!("cargo:rerun-if-changed={}", src);
-    let file = std::fs::File::open(src).unwrap();
-    let spec = serde_json::from_reader(file).unwrap();
+    let spec = include_str!("swagger.3.json");
+    let spec = serde_json::from_str(spec).unwrap();
     let mut generator = progenitor::Generator::default();
-
     let tokens = generator.generate_tokens(&spec).unwrap();
     let ast = syn::parse2(tokens).unwrap();
     let content = prettyplease::unparse(&ast);
-
     let mut out_file = std::path::Path::new(&std::env::var("OUT_DIR").unwrap()).to_path_buf();
     out_file.push("lib.rs");
-
     std::fs::write(out_file, content).unwrap();
 }
