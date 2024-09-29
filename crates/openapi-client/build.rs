439656334bb0fa82914bff8dc78649a1dfe4208d
dzmitry-lahoda <dzmitry@lahoda.pro>
2024-09-29 18:32:43 +0100

    publush

---
+++ crates/openapi-client/build.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let src = "../../src/docs/swagger.3.json";
+    println!("cargo:rerun-if-changed={}", src);
+    let file = std::fs::File::open(src).unwrap();
+    let spec = serde_json::from_reader(file).unwrap();
+    let mut generator = progenitor::Generator::default();
+
+    let tokens = generator.generate_tokens(&spec).unwrap();
+    let ast = syn::parse2(tokens).unwrap();
+    let content = prettyplease::unparse(&ast);
+
+    let mut out_file = std::path::Path::new(&std::env::var("OUT_DIR").unwrap()).to_path_buf();
+    out_file.push("lib.rs");
+
+    std::fs::write(out_file, content).unwrap();
+}
