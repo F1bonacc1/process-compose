6f084978406c4657d8cf01289abb6c03bebf0b55
dzmitry-lahoda <dzmitry@lahoda.pro>
2025-02-07 13:35:36 +0000

    update rust crate to 1.46

--- crates/openapi-client/build.rs
+++ crates/openapi-client/build.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let src = "swagger.json";
+    let src = "../../src/docs/swagger.json";
     println!("cargo:rerun-if-changed={}", src);
-    let spec = include_str!("swagger.json");
+    let spec = include_str!("../../src/docs/swagger.json");
     let spec = serde_json::from_str(spec).unwrap();
     let mut generator = progenitor::Generator::default();
     let tokens = generator.generate_tokens(&spec).unwrap();
