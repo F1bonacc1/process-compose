e81e54906dd09774a3faf813e516c82a82d170da
Berger Eugene <bergere@gmail.com>
2025-05-06 00:01:16 +0300

    chore: bump nix to v1.63.0

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@ let pkg = "github.com/f1bonacc1/process-compose/src/config";
 in
 buildGoModule rec {
   pname = "process-compose";
-  version = "1.46.0";
+  version = "1.63.0";
   go = pkgs.go_1_23;
   env.CGO_ENABLED = 0;
 
