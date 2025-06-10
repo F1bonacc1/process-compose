faeaf3f554af04ec546a8fc96783f527b75f9c12
Juan C. Müller <me@juancmuller.com>
2025-06-10 14:52:57 -0400

    fix: deduplicate status retrievals
    Signed-off-by: Juan C. Müller <me@juancmuller.com>

--- src/client/status.go
+++ src/client/status.go
@@ -20,8 +20,11 @@ func (p *PcClient) isAlive() error {
 	return nil
 }
 
-func (p *PcClient) getHostName() (string, error) {
-	url := fmt.Sprintf("http://%s/hostname", p.address)
+func (p *PcClient) getHostName() (string, error) { return p.getAttribute("hostname", "name") }
+func (p *PcClient) getName() (string, error)     { return p.getAttribute("name", "projectName") }
+
+func (p *PcClient) getAttribute(path, name string) (string, error) {
+	url := fmt.Sprintf("http://%s/%s", p.address, path)
 	resp, err := p.client.Get(url)
 	if err != nil {
 		return "", err
@@ -37,25 +40,5 @@ func (p *PcClient) getHostName() (string, error) {
 		log.Err(err).Send()
 		return "", err
 	}
-	return nameMap["name"], nil
-}
-
-func (p *PcClient) getName() (string, error) {
-	url := fmt.Sprintf("http://%s/name", p.address)
-	resp, err := p.client.Get(url)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("unexpected status %s", resp.Status)
-	}
-
-	nameMap := map[string]string{}
-	//Decode the data
-	if err = json.NewDecoder(resp.Body).Decode(&nameMap); err != nil {
-		log.Err(err).Send()
-		return "", err
-	}
-	return nameMap["projectName"], nil
+	return nameMap[name], nil
 }
