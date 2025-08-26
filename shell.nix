a22f9acf0650e4e4f188de5d7435c4189c32820e
dzmitry-lahoda <dzmitry@lahoda.pro>
2025-08-26 13:28:55 +0100

    nix maintain

--- shell.nix
+++ shell.nix
@@ -2,11 +2,19 @@
   pkgs ? import <nixpkgs> { },
 }:
 pkgs.mkShell {
-  packages = with pkgs; [
-    go
-    gopls
-    gotools
-    gnumake
-    gnused
-  ];
+  packages =
+    let
+      goMod = builtins.readFile ./go.mod;
+      goLine = builtins.elemAt (pkgs.lib.splitString "\n" goMod) 2;
+      goLineMapped = builtins.replaceStrings [ " " "." ] [ "_" "_" ] goLine;
+      go = pkgs."${goLineMapped}";
+    in
+    with pkgs;
+    [
+      go
+      gopls
+      gotools
+      gnumake
+      gnused
+    ];
 }
