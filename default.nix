cfd3801af8f6704daacc16d1b16edba79c75c087
Berger Eugene <bergere@gmail.com>
2025-11-15 22:56:27 +0200

    chore: update version to 1.78.0 in default.nix

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@ let pkg = "github.com/f1bonacc1/process-compose/src/config";
 in
 buildGoModule rec {
   pname = "process-compose";
-  version = "1.76.1";
+  version = "1.78.0";
   go = pkgs.go_1_24;
   env.CGO_ENABLED = 0;
 
