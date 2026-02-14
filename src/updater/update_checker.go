f942109e4b489dc510889a384ca5e9ee89480516
Berger Eugene <bergere@gmail.com>
2026-02-15 01:24:15 +0200

    chore: Update release check URL and add User-Agent header to bypass request blocking.

--- src/updater/update_checker.go
+++ src/updater/update_checker.go
@@ -7,7 +7,8 @@ import (
 
 const (
 	//UrlPath for retrieving the latest release version
-	UrlPath = "https://api.github.com/repos/f1bonacc1/process-compose/releases/latest"
+	// UrlPath = "https://api.github.com/repos/f1bonacc1/process-compose/releases/latest"
+	UrlPath = "https://shr.pn/process-compose-latest"
 )
 
 type Release struct {
@@ -15,7 +16,21 @@ type Release struct {
 }
 
 func GetLatestReleaseName() (string, error) {
-	resp, err := http.Get(UrlPath)
+
+	// 1. Create a custom client
+	client := &http.Client{}
+
+	// 2. Create the request
+	req, err := http.NewRequest(http.MethodGet, UrlPath, nil)
+	if err != nil {
+		return "", err
+	}
+
+	// This makes ShortPen see the call as coming from a real Chrome browser
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+
+	// 4. Execute the request
+	resp, err := client.Do(req)
 	if err != nil {
 		return "", err
 	}
